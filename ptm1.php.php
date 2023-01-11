5635bde7f51e1dcd91c892cfe9e79d3356fe9789
Htognin <119645697+Htognin@users.noreply.github.com>
2023-01-11 15:12:51 +0700

    Add files via upload

---
+++ ptm1.php.php
@@ -0,0 +1,56 @@
+<?php
+
+// /*
+// komentar b anyak baris
+// */
+// // buat komentar
+// echo "hello world <br>";
+// // echo "Saya manusia";
+// // variabel  boleh mengandung angka namunm tidak boleh di depan  contoh :b898sh
+
+
+// // standar output
+// // echo untuk mencetak variabel
+// // print untuk mencetak variabel
+// // var_dump :buat dbugging (buat memunculkan isi variabel atau array)
+// $adcom = "hello world <br>";
+// echo $adcom;
+
+// $adcom = "jum'at";
+
+// // operator aritmatika +,-,/,%
+// $x = 5;
+// $i = 5;
+// echo $i + $x ;
+
+// echo "<br>";
+
+// // operator anssigment
+// // yaitu : =,+=,-=,*=,/=
+// $x = 5;
+// $x = 5;
+// echo $x;
+// echo "<br>";
+// // kenapa hanya tampil angka 5? karena yang dipanggil hanya variabel x saja
+
+// $y = 5;
+// $y *= 6;
+// echo $y;
+// echo "<br>";
+
+// // operator identitas
+// // ===,!===
+
+
+// // concat : penggabungan variabel
+// $nama_depan  = "android";
+// $nama_belakang = "computer";
+
+// echo $nama_depan . " " . $nama_belakang. "<br>";
+
+
+
+// for($i =0; $i) <10;
+
+
+?>
