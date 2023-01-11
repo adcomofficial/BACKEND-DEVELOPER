10aed9c69365b247edc9c91c5bbf85df4d6fc923
rochmanhidayat <112696641+rochmanhidayat@users.noreply.github.com>
2023-01-11 15:14:21 +0700

    Add files via upload

---
+++ ptm-1php/ptm-1.php
@@ -0,0 +1,70 @@
+<?php
+
+//$nama_depan = "android";
+//$nama_belakang = "community";
+//echo $nama_depan. " " .$nama_belakang;
+
+//operator  perbandingan
+//<,>,<=,=>,== !==
+//var_dump(4==4);
+
+//logika
+//&&,||, !
+//$x = 10
+//echo($x < 25 || $x % 2 == 0);
+
+//perulangan
+//for
+//while
+//do...while
+//foreach
+
+//for($i = 0; $i < 10; $i++ ) {
+//        echo "adcom keren <br>";
+//}
+
+//$i = 0;
+//while($i < 10){
+//       echo "adcom keren! <br>";
+//        $i++;
+//}
+
+//$i = 0;
+//do{
+//    echo "adcom keren <br>!";
+//    $i++;
+//}while($i < 10);
+
+
+//pengkondisian
+//if
+//if..else if...else
+//ternary
+//switch
+//$i =35;
+//if($i < 30 ){
+//        echo "benar";
+//}else if($i===30){
+
+//    echo "Draw";
+//}else{
+//    echo "salah";
+//}
+
+//switch
+// $warna ="merah"
+// switch($warna){
+//     case "merah" :
+//         echo "berhenti";
+//         break;
+//     case "kuning" :
+//         echo "hati-hati";
+//         break;
+//     case "hijau";
+//     echo "jalan";
+//     break;
+//     default :
+//     echo "lampu rusak";
+// }
+
+?>
