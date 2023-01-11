d82f2cae4f0b40bf865217d6189bd4e00c0c747e
Muhammad Enricco Rizqy <112739071+enriccoooo@users.noreply.github.com>
2023-01-11 15:11:18 +0700

    Add files via upload

---
+++ adcom/ptm2.php
@@ -0,0 +1,113 @@
+<?php
+
+// Ini buat komentar
+
+/* 
+
+Komentar Buat Banyak Baris
+
+*/
+
+
+
+
+// Standar Output
+// echo
+// print
+// print_r
+// var_dump
+// echo "Hello Wolrd!";
+// Varible Tidak Boleh Di Awali Dengan Angka,Namun Boleh Mengandung Angka
+// $adcom = "Hello World";
+// $adcom = "Jum'at";
+// echo $adcom;
+
+// Operator Aritmatika
+// +,-,*,/,%
+
+// Operetor Assigmment
+// =,+=,-=,*=,/=,%=,!=,.=
+
+// Operator Identitas
+// ===, !===
+// var_dump(4==="4");
+
+// Concat
+// $nama_depan = "Android";
+// $nama_belakang = "Community";
+// echo $nama_depan. " " .$nama_belakang;
+
+// Operator Perbandingan
+// <,>,<=,=>,== !==
+// var_dump(4==="4");
+
+// Logika
+// &&,||, !
+// $x = 30;
+// echo($x < 25 || $x % 2 == 0);
+
+// Perulangan
+// for
+// while
+// do...while
+// foreach
+
+// for($i = 0; $i < 10; $i++) {
+//          echo "Adcom Keren <br>";
+// }
+
+// $i = 10;
+// while($i < 10){
+//     echo "Adcom Keren! <br>";
+//   $i++;
+// }
+
+// $i = 10;
+// do{
+//     echo "Adcom Keren  <br>";
+//     $i++;
+// }while($i < 10);
+
+
+// Pengkodisian
+// if
+// if..else if...else
+// ternary
+// switch
+// $i =35;
+// if($i < 30 ){
+//     echo "Benar";
+// }else if($i===30){
+//     echo "Draw";
+// }else{
+//     echo "Salah";
+// }
+
+
+
+//Switch 
+// $warna ="Merah";
+// switch($warna){
+//     case "Merah" :
+//         echo "Berhenti";
+    
+//     case "Kuning" :
+//         echo "Hati-Hati";
+//         break;
+//     case "Hijau";
+//     echo "Jalan";
+//     break;
+//     default :
+//     echo "Lampu Rusak";
+
+// Farhan
+
+
+
+
+// Challange,Buatlah Switch Dengan Case/Kasus Yang Berbeda
+// Reward Buat 1 Orang,Saya Teraktir Makan,Ayam Geprek IAIN
+// Batas 18 Jan 2023
+
+
+?>
